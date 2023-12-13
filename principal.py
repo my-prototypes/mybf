df375fa828a10730328fa12ac0a4a111c9bbe2d0
armandossrecife <armando.sousa@gmail.com>
2023-12-13 11:33:25 -0300

    Add form to update image

--- principal.py
+++ principal.py
@@ -139,6 +139,41 @@ def profile():
     return render_template("dashboard/profile.html", usuario = session['username'], 
             profilePic="", titulo="Profile", nome=usuario.name, id=str(usuario.id), email=usuario.email)
 
+@login_required
+@app.route("/usuario/<int:id>", methods=['GET', 'PUT'])
+def update(id):    
+    if request.method == "POST":
+        username = request.form["username"]
+        #file_name_to_store = "picture-" + str(id) + ".png" 
+        error = None
+
+        if not username:
+            error = "Username is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            try:
+                print('Processamento do upload da imagem')
+                # TO DO: isolar o tratamento de arquivo
+                #file_image = request.files["image"]
+                #path_to_save = Constant.PATH_UPLOADS + "/" + file_name_to_store
+                # Salva o arquivo no diretorio de uploads
+                #file_image.save(path_to_save)
+            except:
+                error_processing_upload = "Erro no processamento do upload do processamento da imagem."
+                if error_processing_upload is not None:
+                    flash(error_processing_upload, 'danger')
+                    return redirect(url_for("usuario.listar_usuarios"))
+            message = "Usuario atualizado com sucesso!"
+            flash(message, 'success')
+            return redirect(url_for("dashboard.profile"))
+
+    usuario = userDAO.read_user_by_username(username=session['username'])
+
+    return render_template("usuarios/imagem.html", usuario = session['username'], 
+            profilePic="", titulo="Update image", usuario_logado=session['username'], nome=usuario.username)
+
 # Recria as tabelas do banco
 #with app.app_context():
 #    print('Cria as tabelas do banco')
